20c11eae927f91608a3a928f78b700f4348bf3fd
Mohammad Shahid <112233075+Mohammad-Shahid-07@users.noreply.github.com>
2023-06-04 20:33:20 +0530

    config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,12 +4,7 @@ import 'firebase/auth'
 import 'firebase/storage'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDNp6FQ_BFROYJijSRgo_ZxinZP503DAEU",
-  authDomain: "workday-warriors.firebaseapp.com",
-  projectId: "workday-warriors",
-  storageBucket: "workday-warriors.appspot.com",
-  messagingSenderId: "894945792928",
-  appId: "1:894945792928:web:745761c4a556ba2171cac5"
+//  Put Your Own FirebaseConfig Object files Here
 };
 
 // init firebase
